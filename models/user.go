d2668a59a7d2a0c45f4f816e6aafe7434fe68164
irfanamu <irfan@valuecreatives.com>
2022-11-22 11:14:33 +0530

    user request and response is added

--- models/user.go
+++ models/user.go
@@ -3,30 +3,47 @@ package models
 import "time"
 
 type UserInfo struct {
-	Id        int
-	UserId    string
-	FirstName string
-	LastName  string
-	Email     string
+	Id              int    `json:"id,omitempty"`
+	UserId          string `json:"user_id,omitempty"`
+	FirstName       string `json:"first_name,omitempty"`
+	LastName        string `json:"last_name,omitempty"`
+	Email           string `json:"email,omitempty"`
+	Passowrd        string `json:"passowrd,omitempty"`
+	ConfirmPassword string `json:"confirm_password,omitempty"`
 
-	createdAt time.Time
-	updatedAt time.Time
+	createdAt time.Time `json:"created_at,omitempty"`
+	updatedAt time.Time `json:"updated_at,omitempty"`
 }
 
 type UserToken struct {
-	UserId       string
-	RefreshToken string
-	ActiveToken  string
+	UserId       string `json:"user_id,omitempty"`
+	RefreshToken string `json:"refresh_token,omitempty"`
+	ActiveToken  string `json:"active_token,omitempty"`
 
-	createdAt time.Time
-	updatedAt time.Time
+	createdAt time.Time `json:"created_at,omitempty"`
+	updatedAt time.Time `json:"updated_at,omitempty"`
 }
 
 type UserAvatar struct {
-	UserId   string
-	AvatarId string
-	ImageUrl string
+	UserId   string `json:"user_id,omitempty"`
+	AvatarId string `json:"avatar_id,omitempty"`
+	ImageUrl string `json:"image_url,omitempty"`
 
-	createdAt time.Time
-	updatedAt time.Time
+	createdAt time.Time `json:"created_at,omitempty"`
+	updatedAt time.Time `json:"updated_at,omitempty"`
+}
+
+type UserLogin struct {
+	Email    string `json:"email,omitempty"`
+	Password string `json:"password,omitempty"`
+}
+
+type UserResponse struct {
+	Id           int    `json:"id,omitempty"`
+	UserId       string `json:"user_id,omitempty"`
+	Email        string `json:"email,omitempty"`
+	RefreshToken string `json:"refresh_token,omitempty"`
+	ActiveToken  string `json:"active_token,omitempty"`
+	AvatarId     string `json:"avatar_id,omitempty"`
+	ImageUrl     string `json:"image_url,omitempty"`
 }
--- user/service.go
+++ user/service.go
@@ -1,15 +1,19 @@
 package user
 
-import "context"
+import (
+	"context"
+
+	"github.com/IRFAN374/upsvc/models"
+)
 
 type User interface {
-	Register(ctx context.Context) (err error)
-	Login(ctx context.Context) (err error)
-	GetUserById(ctx context.Context) (err error)
-	GetUserByName(ctx context.Context) (err error)
-	GetUserByEmail(ctx context.Context) (err error)
+	Register(ctx context.Context, userInfo models.UserInfo) (userId string, err error)
+	Login(ctx context.Context, Email string, password string) (userRes models.UserResponse, err error)
+	GetUserById(ctx context.Context, UserId string) (userRes models.UserResponse, err error)
+	GetUserByName(ctx context.Context, UserName string) (userRes models.UserResponse, err error)
+	GetUserByEmail(ctx context.Context, Email string) (userRes models.UserResponse, err error)
 
-	UpdateUser(ctx context.Context) (err error)
-	DeleteUser(ctx context.Context) (err error)
-	IsExist(ctx context.Context) (oki bool, err error)
+	UpdateUser(ctx context.Context, userId string) (userRes models.UserResponse, err error)
+	DeleteUser(ctx context.Context, userId string) (ok bool, err error)
+	IsExist(ctx context.Context, userId string) (ok bool, err error)
 }
